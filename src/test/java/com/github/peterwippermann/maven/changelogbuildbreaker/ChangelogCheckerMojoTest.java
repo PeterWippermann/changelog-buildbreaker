addc171c025ec7e6633f5b085777d5382869ef02
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2017-11-24 18:31:08 +0100

    Create unit tests for `ChangelogCheckerMojo`

--- src/test/java/com/github/peterwippermann/maven/changelogbuildbreaker/ChangelogCheckerMojoTest.java
+++ src/test/java/com/github/peterwippermann/maven/changelogbuildbreaker/ChangelogCheckerMojoTest.java
@@ -1,8 +1,11 @@
 package com.github.peterwippermann.maven.changelogbuildbreaker;
 
+import org.apache.maven.plugin.AbstractMojoExecutionException;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.io.File;
 import java.net.URISyntaxException;
@@ -10,67 +13,98 @@ import java.net.URL;
 
 import static com.github.peterwippermann.maven.changelogbuildbreaker.ChangelogCheckerMojo.DEFAULT_PATTERN_FOR_UNRELEASED_CHANGES;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * @author jholtkamp
  */
 public class ChangelogCheckerMojoTest {
 
-    private ChangelogCheckerMojo sut;
+	private ChangelogCheckerMojo sut;
 
-    @Before
-    public void setUp() throws Exception {
-        sut = new ChangelogCheckerMojo();
+	@Before
+	public void setUp() throws Exception {
+		sut = new ChangelogCheckerMojo();
 
-        sut.setUnreleasedChangesPattern(DEFAULT_PATTERN_FOR_UNRELEASED_CHANGES);
-    }
+		sut.setUnreleasedChangesPattern(DEFAULT_PATTERN_FOR_UNRELEASED_CHANGES);
+	}
 
-    @Test
-    public void testExecute_nearlyEmptyFile() throws Exception {
-        sut.setChangelogFile(getFile("CHANGELOG-nearly-empty.MD"));
+	@Test
+	public void testExecute_fileNotSet() throws Exception {
+		sut.setChangelogFile(null);
 
-        sut.execute();
-    }
+		assertException(MojoExecutionException.class, "path to the changelog file has not been set");
+	}
 
-    @Test
-    public void testExecute_emptyUnreleased() throws Exception {
-        sut.setChangelogFile(getFile("CHANGELOG-empty-unreleased.MD"));
+	@Test
+	public void testExecute_missingFile() throws Exception {
+		sut.setChangelogFile(new File("this-file-does-not.exist"));
 
-        sut.execute();
-    }
+		assertException(MojoExecutionException.class, "does not exist");
+	}
 
-    @Test
-    public void testExecute_nonEmptyUnreleased() throws Exception {
-        sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased.MD"));
+	@Test
+	public void testExecute_fileCannotBeRead() throws Exception {
+		File file = Mockito.mock(File.class);
 
-        // TODO this would be prettier with Java 8:
-        // assertThatThrownBy(() -> sut.execute()).isInstanceOf(MojoFailureException.class)
-        //        .hasMessageContaining("forget to update the changelog");
-        try {
-            sut.execute();
-        } catch (MojoFailureException e) {
-            assertThat(e).hasMessageContaining("forget to update the changelog");
-        }
-    }
+		when(file.canRead()).thenReturn(false);
+		when(file.exists()).thenReturn(true);
 
-    @Test
-    public void testExecute_nonEmptyUnreleased_noStartingBlankLine() throws Exception {
-        sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased-missing-starting-new-line.MD"));
+		sut.setChangelogFile(file);
 
-        // FIXME this should fail exactly as in testExecute_nonEmptyUnreleased
-        sut.execute();
-    }
+		assertException(MojoExecutionException.class, "can't be read");
+	}
 
-    @Test
-    public void testExecute_nonEmptyUnreleased_noEndingBlankLine() throws Exception {
-        sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased-missing-ending-new-line.MD"));
+	@Test
+	public void testExecute_nearlyEmptyFile() throws Exception {
+		sut.setChangelogFile(getFile("CHANGELOG-nearly-empty.MD"));
 
-        // FIXME this should fail exactly as in testExecute_nonEmptyUnreleased
-        sut.execute();
-    }
+		sut.execute();
+	}
 
-    private File getFile(String filename) throws URISyntaxException {
-        URL resource = this.getClass().getClassLoader().getResource(filename);
-        return resource != null ? new File(resource.toURI()) : null;
-    }
+	@Test
+	public void testExecute_emptyUnreleased() throws Exception {
+		sut.setChangelogFile(getFile("CHANGELOG-empty-unreleased.MD"));
+
+		sut.execute();
+	}
+
+	@Test
+	public void testExecute_nonEmptyUnreleased() throws Exception {
+		sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased.MD"));
+
+		assertException(MojoFailureException.class, "forget to update the changelog");
+	}
+
+	@Test
+	public void testExecute_nonEmptyUnreleased_noStartingBlankLine() throws Exception {
+		sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased-missing-starting-new-line.MD"));
+
+		// FIXME this should fail exactly as in testExecute_nonEmptyUnreleased
+		sut.execute();
+	}
+
+	@Test
+	public void testExecute_nonEmptyUnreleased_noEndingBlankLine() throws Exception {
+		sut.setChangelogFile(getFile("CHANGELOG-non-empty-unreleased-missing-ending-new-line.MD"));
+
+		// FIXME this should fail exactly as in testExecute_nonEmptyUnreleased
+		sut.execute();
+	}
+
+	private File getFile(String filename) throws URISyntaxException {
+		URL resource = this.getClass().getClassLoader().getResource(filename);
+		return resource != null ? new File(resource.toURI()) : null;
+	}
+
+	private void assertException(Class<? extends AbstractMojoExecutionException> ex, String message) {
+		// TODO this would be prettier with Java 8:
+		// assertThatThrownBy(() -> sut.execute()).isInstanceOf(ex).hasMessageContaining(message);
+		try {
+			sut.execute();
+		} catch (Exception e) {
+			assertThat(e).isInstanceOf(ex);
+			assertThat(e).hasMessageContaining(message);
+		}
+	}
 }
